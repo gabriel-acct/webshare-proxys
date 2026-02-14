0073316197e7e08f4489dc9f32b91dfc726571d6
gabriel-acct <gabrielhenrique00mc@gmail.com>
2026-02-14 16:46:34 -0400

    Add CORS headers to responses for improved cross-origin requests handling

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import requests
-from flask import Flask, request
+from flask import Flask, request, make_response
 
 app = Flask(__name__)
+
+@app.after_request
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    return response
 
 @app.route('/')
 def index():
